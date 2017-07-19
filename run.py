f7fc2bf079ab4548003d3d719ad9a27f22c26ccc
miao <psyviccccc@gmail.com>
2017-07-19 11:00:25 +0200

    增加弹幕君本地

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 from werkzeug.serving import run_simple
 from werkzeug.wsgi import DispatcherMiddleware
 
+from danmu import makeApp as makeDanmu
 from instance.config import BasicConfig
 from myapp import makeApp as makeMyaap
 from sited import makeApp as makeSited
@@ -9,7 +10,9 @@ from sited import makeApp as makeSited
 if __name__ == '__main__':
     helloWorld = makeMyaap(BasicConfig)
     sited = makeSited(BasicConfig)
+    danmu = makeDanmu(BasicConfig)
     app = DispatcherMiddleware(helloWorld, {
-        '/sited': sited
+        '/sited': sited,
+        '/danmu': danmu
     })
     run_simple('localhost', 5000, app, use_reloader=True, use_debugger=True, use_evalex=True)
