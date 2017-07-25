3220fc0655bd300206438e89f65d0fe66330da33
vitam <psyviccccc@gmail.com>
2017-07-25 11:55:17 +0200

    update girignore

--- run.py
+++ run.py
@@ -6,13 +6,16 @@ from danmu import makeApp as makeDanmu
 from instance.config import BasicConfig
 from myapp import makeApp as makeMyaap
 from sited import makeApp as makeSited
+from test import makeApp as makeTest
 
 if __name__ == '__main__':
     helloWorld = makeMyaap(BasicConfig)
     sited = makeSited(BasicConfig)
     danmu = makeDanmu(BasicConfig)
+    test = makeTest(BasicConfig)
     app = DispatcherMiddleware(helloWorld, {
         '/sited': sited,
-        '/danmu': danmu
+        '/danmu': danmu,
+        '/test' : test
     })
     run_simple('localhost', 5000, app, use_reloader=True, use_debugger=True, use_evalex=True)
