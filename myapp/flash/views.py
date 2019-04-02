7d3d078999199804df84ce0d1552d3dec26a4ee5
ShuXiang Gao <dalao1002@gmail.com>
2019-04-02 15:08:23 +0200

    Delete views.py

--- myapp/flash/views.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, Blueprint
-
-flash = Blueprint('flash', __name__, template_folder="./templates")
-
-
-@flash.route("/flash/")
-def index():
-    return render_template("flash.html")
