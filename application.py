5700c2c6eeb7eee7829f282543d444ccfe37e1ad
daolao <dalao1002@gmail.com>
2019-06-15 02:08:09 +0200

    multiple app

--- application.py
+++ application.py
@@ -1,4 +1,12 @@
-from app.webapp import app
+from werkzeug.wsgi import DispatcherMiddleware
+from app.app import app as frontend
+from api.app import app as backend
+
+application = DispatcherMiddleware(frontend, {
+    '/api':     backend
+})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    from werkzeug.serving import run_simple
+    run_simple('localhost', 5000, application, use_reloader=True,
+               use_debugger=True, use_evalex=True)
