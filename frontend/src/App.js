c2a7c017c55a6fab36eb96fb3910c051e797f749
daolao <dalao1002@gmail.com>
2019-06-16 19:08:50 +0200

    import image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,52 +1,27 @@
+import $ from 'jquery';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { TITLE, BANNER } from './Config';
-import { Header } from './Header';
+import Banner from './assets/images/bg1.jpg';
+import { BANNER, TITLE } from './Config';
 import { Footer } from './Footer';
+import { Header } from './Header';
 import IndexPage from './page/IndexPage';
 
-import $ from 'jquery';
 window.$ = $;
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            test: "Soy Test"
-        };
-    }
-
     componentDidMount() {
         document.title = TITLE;
-        // TODO remove testing code
-        fetch("/api/")
-            .then(response => response.text())
-            .then(text => this.setState({ test: text }));
     }
 
     render() {
         return (
             <div className="App">
-                <div className="main-header-image" style={{ backgroundImage: `url('${BANNER}')` }}></div>
+                <div className="main-header-image" style={{ backgroundImage: `url(${Banner})` }}></div>
                 <Header banner={BANNER} />
                 <div id="main-container" className="concat">
                     {this.props.children || <IndexPage />}
                 </div>
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>Edit <code>src/App.js</code> and save to reload.</p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                    {this.state.test}
-                </header>
                 <Footer />
             </div>
         );
